e3e19671b9e1bf486f6d294332b5224e7b0bc865
michaln8991 <michalnadolny192@gmail.com>
2024-01-26 13:32:02 +0100

    refactored js file

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
-let toggle_button = document.querySelector("#toggle-button");
-let toggleable = document.querySelectorAll(".navbar__list-elem--toggleable");
-toggle_button.addEventListener("click", () => {
-    for (item of toggleable) {
-        if (item.style.display == "block") {
-            item.style.display = "none";
-        } else {
-            item.style.display = "block";
+function addToggleableMenu() {
+    let toggle_button = document.querySelector("#toggle-button");
+    let toggleable = document.querySelectorAll(".navbar__list-elem--toggleable");
+    toggle_button.addEventListener("click", () => {
+        for (item of toggleable) {
+            if (item.style.display == "block") {
+                item.style.display = "none";
+            } else {
+                item.style.display = "block";
+            }
         }
-    }
-});
+    });
+}
+
+addToggleableMenu();
